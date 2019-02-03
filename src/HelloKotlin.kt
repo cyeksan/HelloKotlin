851a7d6a7adeb7db6a870c828ad1ed72f28d4908
Cansu Aktaş <yeksancansu@gmail.com>
2019-02-03 13:43:23 +0300

    Commit with Love <3

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -2,4 +2,16 @@ fun main() {
 
     print("Hello Kotlin")
 
+    val intValue = 2
+
+    print("Hello Kotlin $intValue")
+
+    println(
+        """
+        | x
+        | xxx
+        | xxxxxxxxx
+    """.trimMargin()
+    )
+
 }
