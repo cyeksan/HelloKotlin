4186bd18f406b99f4cfaff00bce5727e17887891
Cansu Aktaş <34041050+cyeksan@users.noreply.github.com>
2019-02-09 12:02:31 +0300

    Commit with Love <3

---
+++ src/HelloKodluyoruz.kt
@@ -0,0 +1,15 @@
+// Code with <3
+
+// Created by Cansu AKTAŞ
+// yeksancansu@gmail.com
+// 9.02.2019 - 11:40
+
+
+fun main(args: Array<String>) {
+
+    val name: String = "Cansu"
+    val surName = "AKTAŞ"
+
+    val age = 28
+    val amount = 2.500
+}
